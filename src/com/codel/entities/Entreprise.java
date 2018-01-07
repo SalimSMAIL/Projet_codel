7e3e89ae6d00f6bdce1219d9e6aa51f273484772
sbenalla <sara.benalla.1992@gmail.com>
2018-01-07 11:31:52 +0100

    Update

---
+++ src/com/codel/entities/Entreprise.java
@@ -0,0 +1,15 @@
+package com.codel.entities;
+
+public class Entreprise extends Contact {
+	private long numstreet;
+
+	public long getNumstreet() {
+		return numstreet;
+	}
+
+	public void setNumstreet(long numstreet) {
+		this.numstreet = numstreet;
+	}
+	
+	
+}
