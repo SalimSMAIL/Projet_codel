329df601ec70d3661d8db9884061cb94af56b188
salimSMAIL <smail.salim93@gmail.com>
2018-01-16 18:08:21 +0100

    maj

--- src/com/codel/utils/HibernateUtility.java
+++ src/com/codel/utils/HibernateUtility.java
@@ -1,21 +1,36 @@
 package com.codel.utils;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtility {
-	 
-    public static SessionFactory factory; 
-    private HibernateUtility() {
-    }
- 
-    public static synchronized SessionFactory getSessionFactory() {
- 
-        if (factory == null) {
-            factory = new Configuration()
-					.configure("hibernate.cfg.xml")
-					.buildSessionFactory();
+	private static SessionFactory sessionFactory;
+	
+	private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+        	Configuration configuration = new Configuration();
+        	configuration.configure("hibernate.cfg.xml");
+        	System.out.println("Hibernate Configuration loaded");
+        	
+        	ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        	System.out.println("Hibernate serviceRegistry created");
+        	
+        	SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        	
+            return sessionFactory;
         }
-        return factory;
+        catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            ex.printStackTrace();
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+	
+	public static SessionFactory getSessionFactory() {
+		if(sessionFactory == null) sessionFactory = buildSessionFactory();
+        return sessionFactory;
     }
 }
