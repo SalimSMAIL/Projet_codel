67ae9ede195992c867617616396b38f7db119039
salimSMAIL <smail.salim93@gmail.com>
2018-02-02 10:24:19 +0100

    maj

--- src/com/codel/test/TestCach.java
+++ src/com/codel/test/TestCach.java
@@ -1,28 +1,36 @@
 package com.codel.test;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.codel.entities.Contact;
-import com.codel.utils.HibernateUtility;
+import com.codel.daos.ContactDAO;
+import com.codel.services.ContactGroupServices;
 
 public class TestCach {
 
 	public static void main(String[] args){
 		
+//		
+//		SessionFactory sessionFactory = HibernateUtility.getSessionFactory();
+//		Session session = sessionFactory.openSession();
+//		session.beginTransaction();
+//		session.get(Contact.class, 1l);
+//		session.getTransaction().commit();
+//		session.close();
+//		
+//		sessionFactory = HibernateUtility.getSessionFactory();
+//		Session session2 = sessionFactory.openSession();
+//		session2.beginTransaction();
+//		session2.get(Contact.class, 1l);
+//		session2.getTransaction().commit();
+//		session2.close();
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ContactDAO contactDAO = (ContactDAO)context.getBean("myContactDAO");
 		
-		SessionFactory sessionFactory = HibernateUtility.getSessionFactory();
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		session.get(Contact.class, 1l);
-		session.getTransaction().commit();
-		session.close();
+		contactDAO.findById(1l);
 		
-		sessionFactory = HibernateUtility.getSessionFactory();
-		Session session2 = sessionFactory.openSession();
-		session2.beginTransaction();
-		session2.get(Contact.class, 1l);
-		session2.getTransaction().commit();
-		session2.close();
+
+		contactDAO.findById(1l);
+		
 	}
 }
